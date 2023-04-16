856a7597d8e320f3cbf61a3b3bbb1eef54f91697
Shalllonz <118684017+Shalllonz@users.noreply.github.com>
2023-04-16 20:57:33 +0200

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,42 @@
+# Imports
+
+import requests
+import time
+import os
+from threading import Thread
+
+# Files it will read from
+
+with open("token.txt", "r") as f:
+    token = f.read()
+
+with open("content.txt", "r") as f:
+    content = f.read()
+
+with open("channel.txt", "r") as f:
+    channel = f.read()
+
+    
+
+
+
+# Payload's And Headers.
+
+payload = {
+    'content': content
+}
+
+header = {
+    'authorization': token
+}
+
+# The loop itself
+
+for i in range (1000):
+    time.sleep(2)
+
+    r = requests.post(channel, json=payload, headers = header)
+
+    print("Message Sent!")
+
+
