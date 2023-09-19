0aa9f3a938d167807c879f09590d55e3936828de
Shalllonz <118684017+Shalllonz@users.noreply.github.com>
2023-09-19 22:35:56 +0200

    ja

---
+++ discord.py
@@ -0,0 +1,38 @@
+import requests
+import os
+import time
+import colorama
+from colorama import Fore, Style, Back
+colorama.init(autoreset=True)
+os.system("cls" or "clear")
+
+with open("token.txt") as f:
+    token = f.read()
+    
+with open("content.txt") as f:
+    content = f.read()
+
+channel = "https://discord.com/api/v9/channels/1/messages"
+channel = channel[:36] + input(Fore.LIGHTYELLOW_EX + "Channel ID?: ") + "/messages"
+speed = float(input(Fore.LIGHTGREEN_EX + "How fast should the messages send (in seconds): "))
+repeat = int(input(Fore.LIGHTRED_EX + "How many times do you wanna send the message: "))
+
+os.system('cls')
+
+header = {
+    'authorization': token
+}
+
+payload = {
+    'content': content
+}
+
+for i in range (repeat):
+    time.sleep(speed)
+
+    r = requests.post(channel, json=payload, headers=header)
+    if r.status_code == requests.codes.ok:
+        print(Fore.GREEN + "Message sent!")
+    else:
+        print(Fore.RED + "You are being ratelimited!")
+        time.sleep(7)
